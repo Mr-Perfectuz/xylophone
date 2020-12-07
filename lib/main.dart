5ccbaa53e14999421af2ce22ef8eb33478a6f897
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-08 00:04:26 +0500

    Audio Player package has been enabled to play

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,65 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note4.wav');
-              },
-              child: Text('Click me'),
-            ),
+          child: Column(
+            children: [
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: Text('Note 1'),
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+                child: Text('Note 2'),
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+                child: Text('Note 3'),
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+                child: Text('Note 4'),
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+                child: Text('Note 5'),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+                child: Text('Note 6'),
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
+                child: Text('Note 7'),
+              ),
+            ],
           ),
         ),
       ),
