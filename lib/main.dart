64cc1119bfcab85a74ce5aac06951f8cd486b632
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-08 14:42:43 +0500

    PlaySound function has been enabled

--- lib/main.dart
+++ lib/main.dart
@@ -9,72 +9,32 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
+  int BuildKey() {
+    Expanded(
+      child: FlatButton(
+        color: Colors.red,
+        onPressed: () {
+          PlaySound(1);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                child: FlatButton(
-                  color: Colors.red,
-                  onPressed: () {
-                    PlaySound(1);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.orange,
-                  onPressed: () {
-                    PlaySound(2);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.yellow,
-                  onPressed: () {
-                    PlaySound(3);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.green,
-                  onPressed: () {
-                    PlaySound(4);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.teal,
-                  onPressed: () {
-                    PlaySound(5);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.blue,
-                  onPressed: () {
-                    PlaySound(6);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.purple,
-                  onPressed: () {
-                    PlaySound(7);
-                  },
-                ),
-              ),
-            ],
-          ),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            BuildKey(),
+            BuildKey(),
+            BuildKey(),
+            BuildKey(),
+            BuildKey(),
+            BuildKey(),
+            BuildKey(),
+          ]),
         ),
       ),
     );
